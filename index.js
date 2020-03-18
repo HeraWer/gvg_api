9524486cb00c962e063978390e4d846abb4d5ff0
vallejestudiante <vallejestudiante@gmail.com>
2020-03-18 11:41:39 +0100

    CORS

--- index.js
+++ index.js
@@ -29,7 +29,16 @@ const PORT = process.env.PORT || 3000;
 
 // Cuando hacemos comando "node index.js" se nos iniciara el puerto 3000 si no esta el otro y lo tendra abierto escuchando
 app.listen(PORT, () => 
-    console.log(`El servidor está inicializado en el puerto ${3000}`));
+    console.log(`El servidor está inicializado en el puerto ${PORT}`));
+
+// Cors
+app.use(function (req, res, next) {
+    res.setHeader('Acces-Control-Allow-Origin', '*');
+    res.setHeader('Acces-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Acces-Control-Allow-Headers', '*');
+    res.setHeader('Acces-Controll-Allow-Credentials', true);
+    next();
+})
 
 // Aqui le decimos que usa la ruta a partir de raiz y lo que se encuentre en el user.route.js
 app.use("/", routeUser);
